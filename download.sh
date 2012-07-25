6d88a547b9d327c20dde298f29cbbb3e7c80274e
Henrik <henrik@haf.se>
2012-07-25 17:22:25 +0200

    better defaults handling

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo -e "Type version to download (at time of writing "0.19.8"): \c"
-read es_version
+default=0.19.8
+read -p "Type ElasticSearch version to download [0.19.8]: " -e es_version
+es_version=${es_version:-$default}
 es_tarchive="elasticsearch-$es_version.tar.gz"
 es_source="http://cloud.github.com/downloads/elasticsearch/elasticsearch"
 curl -o files/$es_tarchive $es_source/$es_tarchive
