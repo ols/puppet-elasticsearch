c7c5edc588595b404491caf452dc7a5ba0ab3616
Henrik <henrik@haf.se>
2012-12-28 18:32:59 +0100

    updating paths

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-default=0.19.8
-read -p "Type ElasticSearch version to download [0.19.8]: " -e es_version
+default=0.20.2
+read -p "Type ElasticSearch version to download [0.20.2]: " -e es_version
 es_version=${es_version:-$default}
 es_tarchive="elasticsearch-$es_version.tar.gz"
-es_source="http://cloud.github.com/downloads/elasticsearch/elasticsearch"
+es_source="http://download.elasticsearch.org/elasticsearch/elasticsearch"
 curl -o files/$es_tarchive $es_source/$es_tarchive
